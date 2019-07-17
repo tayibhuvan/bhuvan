e787b815fa8a6b68d206bf629a8fd21802066aec
tayibhuvan <52479834+tayibhuvan@users.noreply.github.com>
2019-07-16 21:23:16 -0700

    Create leapyear.py

---
+++ leapyear.py
@@ -0,0 +1,11 @@
+a = input()
+ if(a%4 == 0):
+   if(a%100==0):
+     if(a%400):
+       print ("no")
+     else:
+       print("yes")
+     else:
+       print('yes")
+     else:
+       print("no")
